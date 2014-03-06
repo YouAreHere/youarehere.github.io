4ac4536c66f77f92dc3c44b77c866d8142953380
Tai Won Chung <tchung1118@gmail.com>
2014-03-06 01:05:54 -0600

    navigate to place links now working.. somewhat

--- JS/navigate.js
+++ JS/navigate.js
@@ -1,18 +1,20 @@
-var nav_map;
+var nav_map, searchBox;
+var markers=[];
+var default_center = new google.maps.LatLng(42.054647, -87.677139);
+var markerAtCenter = 0;
 function init_navi()
 {
 	if (nav_map == undefined) {
-		var markers=[];
 		var map_canvas = document.getElementById('navMap_canvas');
 		var mapOpts = {
-			center: new google.maps.LatLng(42.054647, -87.677139),
+			center: default_center,
 			zoom: 15,
 			streetViewControl: false,
 			panControl: false,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		nav_map = new google.maps.Map(map_canvas, mapOpts);
-		var searchBox = new google.maps.places.SearchBox(document.getElementById('pac-input'));
+		searchBox = new google.maps.places.SearchBox(document.getElementById('pac-input'));
 		google.maps.event.addListener(searchBox, 'places_changed', function() {
     		var places = searchBox.getPlaces();
 
@@ -58,9 +60,57 @@ function init_navi()
 	}
 }
 
+var address_latlng = [
+	{
+		address: "The Rock Northwestern University Evanston, IL 60208",
+		latlng: new google.maps.LatLng(42.051545, -87.675910)
+	},
+	{
+		address: "The Lakefill Evanston, IL 60201",
+		latlng: new google.maps.LatLng(42.055285, -87.670850)
+	},
+	{
+		address: "Norris University Center 1999 Campus Drive Evanston, IL 60208",
+		latlng: new google.maps.LatLng(42.053332, -87.672744)
+	},
+	{
+		address: "1715 Maple Ave Evanston, IL 60201",
+		latlng: new google.maps.LatLng(42.048967, -87.684637)
+	},
+	{
+		address: "Ryan Field",
+		latlng: new google.maps.LatLng(42.065976, -87.692463)
+	},
+	{
+		address: "Evanston, IL",
+		latlng: new google.maps.LatLng(42.054647, -87.677139)
+	}];
+
 function navigateToPlace(place)
 {
-	nav_map.setCenter(new google.maps.LatLng(42.054647, -87.677139));
+	for (var i = 0, marker; marker = markers[i]; i++) {
+      	marker.setMap(null);
+    }
+
+    // For each place, get the icon, place name, and location.
+    markers = [];
+	if (nav_map == undefined) {
+		default_center = address_latlng.filter(function(element, index, array) {
+			return (element.address == place);
+		})[0].latlng;
+		markerAtCenter = 1;
+	} else {
+		var latlng = address_latlng.filter(function(element, index, array) {
+			return (element.address == place);
+		})[0].latlng;
+		nav_map.setCenter(latlng);
+		var marker = new google.maps.Marker({
+       			 	map: nav_map,
+        			position: latlng
+      			});
+      	nav_map.setZoom(17);
+      	markers.push(marker);
+	}
 }
 
 $(document).on("pageshow","#navigation",
@@ -73,6 +123,14 @@ $(document).on("pageshow","#navigation",
     				//console.log($(this).find('[id="map_canvas"]'));
     				if (nav_map == undefined) {
 						init_navi();
+						if (markerAtCenter) {
+							var marker = new google.maps.Marker({
+       			 				map: nav_map,
+        						position: nav_map.getCenter()
+      						});
+      						nav_map.setZoom(17);
+      						markers.push(marker);
+						}
 					} else {
 						google.maps.event.trigger(nav_map, 'resize');
 					}
@@ -83,5 +141,6 @@ $( document ).ready(function() {
             var $this = $(this);
             var p1 = $this.data('address');
             console.log(p1);
+            navigateToPlace(p1);
         });
 });
