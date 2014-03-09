5775697fc7fb3ad31d679ff765665b6a1d55ce0d
Amanda <amanda.anumba@gmail.com>
2014-03-08 23:40:40 -0600

    design updates

---
+++ JS/calendar.js
@@ -0,0 +1,9 @@
+addthisevent.settings({
+	mouse		: false,
+	css			: false,
+	outlook		: {show:false, text:"Outlook"},
+	google		: {show:false, text:"Google"},
+	yahoo		: {show:false, text:"Yahoo"},
+	ical		: {show:false, text:"iCal"},
+	hotmail		: {show:false, text:"Hotmail"},
+});
