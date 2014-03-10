4996680fd174d33ac148256584e150cd174f9e29
Amanda <amanda.anumba@gmail.com>
2014-03-10 11:03:00 -0500

    fixing the swipe menu

--- JS/menu.js
+++ JS/menu.js
@@ -1,5 +1,5 @@
-$( document ).on( "pagecreate", "#events", function() {
-    $( document ).on( "swipeleft swiperight", "#events", function( e ) {
+$( document ).on( "pagecreate", ".swipe", function() {
+    $( document ).on( "swipeleft swiperight", ".swipe", function( e ) {
         // We check if there is no open panel on the page because otherwise
         // a swipe to close the left panel would also open the right panel (and v.v.).
         // We do this by checking the data that the framework stores on the page element (panel: open).
