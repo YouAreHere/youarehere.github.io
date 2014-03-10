f1f1d2cf9ee1e195476834e79b6a30f03a39e4aa
Amanda <amanda.anumba@gmail.com>
2014-03-10 10:21:17 -0500

    swipe panel updates

--- JS/calendar.js
+++
@@ -1,9 +0,0 @@
-addthisevent.settings({
-	mouse		: false,
-	css			: false,
-	outlook		: {show:false, text:"Outlook"},
-	google		: {show:false, text:"Google"},
-	yahoo		: {show:false, text:"Yahoo"},
-	ical		: {show:false, text:"iCal"},
-	hotmail		: {show:false, text:"Hotmail"},
-});
